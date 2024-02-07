79e41e7e82458f2b4e1a6ba4ea61a04e54e2a57e
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-02-07 21:41:11 +0100

    add passwordless ssh for nix user and fix ocr1

--- nixos/_mixins/users/nix/default.nix
+++ nixos/_mixins/users/nix/default.nix
@@ -1,5 +1,11 @@
-_:
+{pkgs, ...}:
 {
+
+	environment.systemPackages = with pkgs; [
+		vim
+	];
+
+	
 
 	users.groups.nix = {};
   users.users.nix = {
@@ -10,4 +16,14 @@ _:
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA0wY1HBFWJGgaoT0L23bQg3icnmyDBds12gc0iOzuDV yohan@laptop-nix"
     ];
   };
+
+	security.sudo.extraRules= [
+		{  users = [ "nix" ];
+			commands = [
+				 { command = "ALL" ;
+					 options= [ "NOPASSWD" ]; # "SETENV" # Adding the following could be a good idea
+				}
+			];
+		}
+	];
 }
