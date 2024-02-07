06ee89540717f5475a0ecbee907dc917fe7bb24a
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-02-07 20:03:05 +0100

    add nix group

--- nixos/_mixins/users/nix/default.nix
+++ nixos/_mixins/users/nix/default.nix
@@ -1,6 +1,9 @@
 _:
 {
+
+	users.groups.nix = {};
   users.users.nix = {
+		group = "nix";
     hashedPassword = null;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA0wY1HBFWJGgaoT0L23bQg3icnmyDBds12gc0iOzuDV yohan@laptop-nix"
