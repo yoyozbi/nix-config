c1a2379a28e676a487f4f7f2260520ba1681f227
yoyozbi <46626633+yoyozbi@users.noreply.github.co>
2024-04-25 23:08:55 +0200

    add rp to cachix

--- flake.nix
+++ flake.nix
@@ -85,6 +85,7 @@
             "tiny1" = self.nixosConfigurations."tiny1".config.system.build.toplevel;
             "tiny2" = self.nixosConfigurations."tiny2".config.system.build.toplevel;
             "ocr1" = self.nixosConfigurations."ocr1".config.system.build.toplevel;
+            "rp" = self.nixosConfigurations."rp".config.system.build.toplevel;
           };
         };
       };
