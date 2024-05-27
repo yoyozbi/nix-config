fe5b732959b1e8596829bab71a57162e3065bb50
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-05-27 15:11:38 +0200

    use stable version of hyprland

--- nixos/_mixins/desktop/hyprland.nix
+++ nixos/_mixins/desktop/hyprland.nix
@@ -43,7 +43,7 @@ in
   programs = {
     hyprland = {
       enable = true;
-      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+#      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     };
     bash = {
       interactiveShellInit = ''
