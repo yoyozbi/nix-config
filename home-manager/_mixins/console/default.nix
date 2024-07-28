458732ed7fc87df284b95e9ab45ba97368762fc1
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-07-28 15:58:05 +0200

    add lua language-server

--- home-manager/_mixins/console/default.nix
+++ home-manager/_mixins/console/default.nix
@@ -28,6 +28,7 @@ in
       [
         (nerdfonts.override { fonts = [ "FiraCode" "SourceCodePro" "JetBrainsMono" "UbuntuMono" ]; })
         iosevka-comfy.comfy
+				lua-language-server
 
         # lsp
         tree-sitter
