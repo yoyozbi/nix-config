31edfa39246c23826a9306e4a63c7d40512d64f7
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-09-28 12:33:07 +0200

    readd k3s

--- nixos/rp/default.nix
+++ nixos/rp/default.nix
@@ -6,7 +6,7 @@
     (modulesPath + "/installer/scan/not-detected.nix")
     ../_mixins/services/openssh.nix
     ../_mixins/services/cachix.nix
-    #./k3s.nix
+    ./k3s.nix
   ];
 
   boot.loader.grub.enable = false;
@@ -52,7 +52,6 @@
   };
 
   swapDevices = [ ];
-	services.k3s.enable = false;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
