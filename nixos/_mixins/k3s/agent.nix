818556ef07d7ab091de65e1a5db1063245254fe9
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-10-02 14:32:28 +0200

    don't add public ip for worker nodes

--- nixos/_mixins/k3s/agent.nix
+++ nixos/_mixins/k3s/agent.nix
@@ -7,7 +7,6 @@
   services.k3s = {
     role = "agent";
     extraFlags = toString [
-      "--node-external-ip=${config.networking.yoyozbi.currentHost.externalIp}"
       "--node-ip=${config.networking.yoyozbi.currentHost.internalIp}"
     ];
     serverAddr = lib.mkDefault "https://${config.networking.yoyozbi.hosts.ocr1.internalIp}:6443";
