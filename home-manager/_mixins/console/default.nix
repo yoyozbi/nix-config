b0eefa50b34f76256b66427f5e009ffcd7e03418
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-10-15 22:15:05 +0200

    add a zsh alias to delete old git branches

--- home-manager/_mixins/console/default.nix
+++ home-manager/_mixins/console/default.nix
@@ -201,6 +201,7 @@ in
         more = "bat";
         checkip = "curl -s ifconfig.me/ip";
         tree = "eza --tree";
+				delete-old-branch = "git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == \"[gone]\" {sub(\"refs/heads/\", \"\", $1); print $1}'); do git branch -D $branch; done";
       };
       oh-my-zsh = {
         enable = true;
