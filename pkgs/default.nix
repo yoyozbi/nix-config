dcebeefcf2040d0766ce4bcd42d50090b08e21d9
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-10-24 15:25:00 +0200

    add zed-editor

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,9 +1,10 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # Build them using 'nix build .#example' or (legacy) 'nix-build -A example'
-{ pkgs ? (import ../nixpkgs.nix) { } }: {
+{ pkgs ? (import ../nixpkgs.nix) { }}: {
   #distrobox = pkgs.callPackage ./distrobox.nix { };
   kDrive = pkgs.callPackage ./kDrive { };
   sddm-themes = pkgs.callPackage ./sddm-themes.nix { };
   eclipse202306 = pkgs.callPackage ./eclipse-202306 { };
   superProductivity = pkgs.callPackage ./superProductivity.nix { };
+	zed-editor = pkgs.callPackage ./zed-editor { };
 }
