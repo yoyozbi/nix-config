95c632e6f7e9b3a487f44130d080613f7a80dff6
yoyozbi <yohan.zbinden@gmail.com>
2024-11-16 13:46:46 +0100

    try to fix cluster agents

--- nixos/_mixins/k3s/agent.nix
+++ nixos/_mixins/k3s/agent.nix
@@ -7,8 +7,8 @@
   services.k3s = {
     role = "agent";
     extraFlags = toString [
-      "--node-external-ip=${config.networking.yoyozbi.currentHost.externalIp}"
-      "--node-ip=${config.networking.yoyozbi.currentHost.internalIp}"
+      #"--node-external-ip=${config.networking.yoyozbi.currentHost.externalIp}"
+      #"--node-ip=${config.networking.yoyozbi.currentHost.internalIp}" # Apparently this causes gateway errors when querying logs
     ];
     serverAddr = lib.mkDefault "https://${config.networking.yoyozbi.hosts.ocr1.internalIp}:6443";
     tokenFile = config.sops.secrets.k3s-server-token.path;
