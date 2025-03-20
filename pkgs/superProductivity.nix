37d85ff30ec0970172f8dcd268c6d7e9b6a94871
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2025-03-20 09:31:39 +0100

    upgrade superProd

--- pkgs/superProductivity.nix
+++ pkgs/superProductivity.nix
@@ -8,11 +8,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "super-productivity";
-  version = "11.1.3";
+  version = "12.0.1";
 
   src = fetchurl {
     url = "https://github.com/johannesjo/super-productivity/releases/download/v${version}/superProductivity-x86_64.AppImage";
-    sha256 = "aRK/grVx9thwf/UG/SQpH6jfEL4dIO7XVeoFWEuVZ2Y=";
+    sha256 = "nNqNnoGzeIu21iLEDcD39s0sH3gP1adiUqt/bheooIM=";
     name = "${pname}-${version}.AppImage";
   };
 
