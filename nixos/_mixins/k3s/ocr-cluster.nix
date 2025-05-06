1cb6f0d0a71322f83488125b5a9e2a15079f4ef1
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2025-05-06 13:51:16 +0200

    add all secrets to ocr-cluster

--- nixos/_mixins/k3s/ocr-cluster.nix
+++ nixos/_mixins/k3s/ocr-cluster.nix
@@ -5,5 +5,15 @@ _: {
       path = "/var/lib/netdata/cloud.d/token";
     };
     k3s-server-token = {};
+    "homepage/cachix-api-key" = {};
+    "homepage/argocd-api-key" = {};
+    "homepage/cloudflare-account-id" = {};
+    "homepage/cloudflare-api-key" = {};
+    "homepage/jellyfin-api-key" = {};
+    "homepage/jellyseerr-api-key" = {};
+    "homepage/radarr-api-key" = {};
+    "homepage/sonarr-api-key" = {};
+    "homepage/qbittorrent-username" = {};
+    "homepage/qbittorrent-password" = {};
   };
 }
