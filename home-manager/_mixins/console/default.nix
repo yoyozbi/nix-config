76cd444bbbb0b31271d0e8de49ca7c7fe420b426
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2025-06-07 12:16:31 +0200

    fix zsh

--- home-manager/_mixins/console/default.nix
+++ home-manager/_mixins/console/default.nix
@@ -215,7 +215,7 @@ in
         tree = "eza --tree";
         delete-old-branch = "git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == \"[gone]\" {sub(\"refs/heads/\", \"\", $1); print $1}'); do git branch -D $branch; done";
       };
-      initExtra = ''
+      initContent = ''
         fastfetch
       '';
       oh-my-zsh = {
